f083bfd1731adce34f08c4d70cd450095547c0b5
Ricky Maisuria <ricky.maisuria@gmail.com>
2021-10-17 14:43:32 +0100

    Commit ya shit

---
+++ project/app/api/ping.py
@@ -0,0 +1,16 @@
+# project/app/api/ping.py
+
+from fastapi import APIRouter, Depends
+
+from app.config import Settings, get_settings
+
+router = APIRouter()
+
+
+@router.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing,
+    }
--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -1,6 +1,7 @@
 # project/app/models/tortoise.py
 
 from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class TextSummary(models.Model):
@@ -10,3 +11,6 @@ class TextSummary(models.Model):
 
     def __str__(self):
         return self.url
+
+
+SummarySchema = pydantic_model_creator(TextSummary)
